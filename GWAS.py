b4915d3d16ec9a26feb52010dfcb441be8cdb8f2
Madison Koelzer <madisonk1998@hotmail.com>
2024-03-04 17:40:10 -0800

    Refactored into GWAS.py and visuals.ipynb

--- GWAS.py
+++ GWAS.py
@@ -1,4 +1,3 @@
-# %%
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,45 +6,10 @@ import pandas as pd
 from tqdm import tqdm
 import statsmodels.api as sm
 from qqman import qqman
+import sys
+import argparse
 
-# %%
-df = pd.read_table('gwas.phen', sep='\s+', header=None)
-phen_values = df[2].to_numpy()
-phen_samples = df[0].to_numpy()
-
-vcf_reader = vcf.Reader(filename="gwas.vcf.gz")
-vcf_samples = []
-
-for record in vcf_reader:
-    for sample in record.samples:
-        vcf_samples.append(sample.sample)
-    break
-
-# %%
-vcf_reader = vcf.Reader(filename="gwas.vcf.gz")
-Xj = []
-
-for record in tqdm(vcf_reader):
-    snp = []
-    for sample in record.samples:
-        gt_alleles = sample.gt_alleles
-        snp.append(int(gt_alleles[0] != '0') + int(gt_alleles[1] != '0')) #(gt_alleles[0] != 0) + ... # 0|2 -> 1
-    Xj.append(snp)
-
-# %%
-organized_phen_samples = []
-organized_phen_values = []
-for vSample in vcf_samples:
-    for i in range(len(phen_samples)):
-        if vSample == phen_samples[i]:
-            organized_phen_samples.append(phen_samples[i])
-            organized_phen_values.append(phen_values[i])
-
-
-# %% [markdown]
-# Our Model: Y = alpha_j + beta_j * X_j + epsilon_j
-
-# %%
+#Deprecated
 class LinearRegressionModel:
     def init(self, alphas, xs, Ys):
         assert(len(alphas) == len(xs) == len(Ys))
@@ -62,8 +26,6 @@ class LinearRegressionModel:
         Beta = (sum_xy - (sum_x_sum_y / n)) / (sum_squared_x - (sum_x_squared / n))        
         return Beta
 
-
-# %%
 #Add column of ones before values
 def add_column_ones(x):
     return np.append(np.atleast_2d(np.ones(len(x))).T, np.atleast_2d(x).T, axis=1)
@@ -77,8 +39,6 @@ def runLinearModel(x, y):
     results = model.fit()
     return results.params[1], results.pvalues[1]
 
-
-# %%
 class GWAS:
     def init(self, linearFunc, gts, pts):
         self.gts = gts
@@ -93,64 +53,77 @@ class GWAS:
             pValues.append(pValue)
         return pValues
 
-# %%
-gwas = GWAS()
-gwas.init(runLinearModel, Xj, organized_phen_values)
-p_values = gwas.get_p_values()
+def main():
+    # Argument Reading
+    try:
+        parser = argparse.ArgumentParser(description='''A Custom GWAS Implementation.''')
+        parser.add_argument("vcf_file", help="A VCF file storing the genotype data for n samples",
+                type=str)
+        parser.add_argument("phen_file", help="A .phen file storing the phenotype data for n samples",
+                type=str)
+        args = parser.parse_args()
 
 
-# %%
-vcf_reader = vcf.Reader(filename="gwas.vcf.gz")
-df = pd.DataFrame(columns=['BP','CHR','P', 'SNP'])
-BP = []
-CHR = []
-SNP = []
+        phen_file = args.phen_file
+        vcf_file = args.vcf_file
+    except:
+        e = sys.exc_info()[0]
+        print(e)
+        return -1
 
-for record in tqdm(vcf_reader):
-    # df1 = pd.DataFrame({'BP':[record.POS],'CHR':[record.CHROM],'P':[p_values[index]],'SNP':[record.ID]})
-    # df = pd.concat([df,df1])
-    BP.append(record.POS)
-    CHR.append(record.CHROM)   
-    SNP.append(record.ID)
+    # File Reading & Data Organization
+    try:
+        print("Reading Input:")
+        df = pd.read_table(phen_file, sep='\s+', header=None)
+        phen_values = df[2].to_numpy()
+        phen_samples = df[0].to_numpy()
+
+        vcf_reader = vcf.Reader(filename=vcf_file)
+        vcf_samples = []
+        BP = []
+        CHR = []
+        SNP = []
+
+        for record in vcf_reader:
+            for sample in record.samples:
+                vcf_samples.append(sample.sample)
+            break
+        
+        vcf_reader = vcf.Reader(filename="gwas.vcf.gz")
+        Xj = []
+
+        for record in tqdm(vcf_reader):
+            snp = []
+            BP.append(record.POS)
+            CHR.append(record.CHROM)   
+            SNP.append(record.ID)
+            for sample in record.samples:
+                gt_alleles = sample.gt_alleles
+                snp.append(int(gt_alleles[0] != '0') + int(gt_alleles[1] != '0')) #(gt_alleles[0] != 0) + ... # 0|2 -> 1
+            Xj.append(snp)
+
+        organized_phen_values = []
+        for vSample in vcf_samples:
+            for i in range(len(phen_samples)):
+                if vSample == phen_samples[i]:
+                    organized_phen_values.append(phen_values[i])
+        print("Input Finished")
+    except:
+        print("Unable to read phenotype and/or genotype data from files.")
+
+    #GWAS
+    try:
+        print("Beginning GWAS")
+        gwas = GWAS()
+        gwas.init(runLinearModel, Xj, organized_phen_values)
+        p_values = gwas.get_p_values()
+        df = pd.DataFrame({'CHR':[int(c) for c in CHR],'BP':BP,'P':p_values, 'SNP':SNP})
+        df.to_csv('output.csv', index=False)
+        print("Gwas finished. Output sent to output.csv")
+    except:
+        print("Unable to perform GWAS. Is your data complete?")
 
 
-# %%
-def QQPlot(pvals):
-    # Sort the observed p-values
-    pvals.sort()
-    # Generate numbers from a uniform distribution
-    n = len(pvals)
-    a = .375 if n<=10 else .5
-    unif = (np.arange(n) + 1 - a)/(n + 1 - 2*a)
 
-    # Make a QQ plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(-1*np.log10(unif), -1*np.log10(pvals), s=5, color="black")
-    ax.plot([0, 6], [0,6])
-    # ax.set_xlim(left=0, right=3)
-    # ax.set_ylim(bottom=0, top=max(-1*np.log10([item for item in pvals if item >0])))
-    ax.set_xlabel("Expected -log10(P)")
-    ax.set_ylabel("Observed -log10(P)")
-
-# %%
-QQPlot(np.sort(p_values))
-
-# %%
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.hist(p_values, alpha=0.5, bins=np.arange(0, 1.05, 0.1))
-ax.set_xlabel("P-value")
-ax.set_ylabel("Frequency")
-
-# %%
-df = pd.DataFrame({'CHR':[int(c) for c in CHR],'BP':BP,'P':p_values, 'SNP':SNP})
-df
-
-# %%
-fig, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [2, 1]})
-fig.set_size_inches((15, 5))
-qqman.manhattan(df, ax=ax0)
-qqman.qqplot(df, ax=ax1)
-
+main()
 
--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 Current Overview:
 
-For our project, we are building our own custom GWAS using linear regression that will take in a simulated phenotype data file and a genotype vcf file and output a data file containing p-values for each SNP in the genotype vcf file. We have a python executable that takes in two system arguments corresponding to each data file. The output will be a dataframe where each row corresponds to a SNP and the generated p-value associated with it. The dataframe can be used to generate qq plots and manhattan plots to visualize the generated values. This is the extent of what we have created so far for our project.
+For our project, we are building our own custom GWAS using linear regression that will take in a simulated phenotype data file and a genotype vcf file and output a data file containing p-values for each SNP in the genotype vcf file. We have a python executable, GWAS.py, that takes in two system arguments corresponding to each data file. The two arguments are vcf_file and phen_file. The vcf_file should store the genotype data for n samples and the phen_file should store the phenotype data for n samples. In our case, we use the simulated phenotype data and the genotype data given to us in problem set 3. This then allows us to compare our custom GWAS with what we did in problem set 3. The executable will read both files in and organize the data so that we can run a linear regression model on the data to obtain p values corresponding to each SNP. We store each value along with its corresponding chromosome, rID, and position into a dataframe. The dataframe is then written to a output.csv file where we can use that data to produce visualizations such as qq plots and manhattan plots as demonstrated in our visuals.ipynb. The following line shows how to run the python executable on the command line given the files gwas.phen and pwas.vcf.gz:
+
+python GWAS.py "gwas.vcf.gz" "gwas.phen"
+
+For help and a description about the python executable, you can run:
+
+python GWAS.py -h
 
 Remaining Work:
 
-We would like to test GWAS using different models rather than just linear regression. We also still need to compare our implementation with that of plink's. We want to compare our results and runtime to then analyze the performance of our model. 
+We would like to test GWAS using different models rather than just linear regression. We also still need to do a deeper comparison of our implementation with that of plink's. We want to compare our results and runtime to then analyze the performance of our model. Something that we noticed was that the runtime of the excecutable took a long time. There are two main portions that take up most of the time: reading the vcf file and running GWAS on the data. We want to be able to decrease the runtime by finding out ways to optimize these two portions. This could be done by possibly parallelizing any parts that we can.
 
 
